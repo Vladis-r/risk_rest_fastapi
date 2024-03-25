d44630d30c37045a6329a69e5b1337425f38fe85
Vladislav <ragozin.vladis@gmail.com>
2024-03-25 13:54:54 +0300

    fixed model and validators and added validation_exception_handler

--- db/models.py
+++ db/models.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-from pydantic import model_validator, field_validator
+from fastapi import HTTPException
+from fastapi.exceptions import RequestValidationError
+from pydantic import model_validator, field_validator, ValidationError, BaseModel
 from sqlmodel import SQLModel, Field
-from typing import Optional
+from typing import Optional, ClassVar
 
 
-class Deposit(SQLModel, table=True):
-    __tablename__ = 'deposit'
-    deposit_id: Optional[int] = Field(default=None, primary_key=True)
+class DepositBase(SQLModel):
     date: str = Field(..., description='Дата заявки')
     periods: int = Field(..., description='Количество месяцев по вкладу')
     amount: int = Field(..., description='Сумма вклада')
@@ -19,26 +19,31 @@ class Deposit(SQLModel, table=True):
         try:
             datetime.strptime(v, '%d.%m.%Y')
         except ValueError:
-            raise ValueError("Invalid date format. Date must be in the format dd.mm.YYYY")
+            raise RequestValidationError("Invalid date format. Date must be in the format dd.mm.YYYY")
         return v
 
     @field_validator("periods")
     @classmethod
     def validate_periods(cls, v: int) -> int:
         if not 1 <= v <= 60:
-            raise ValueError('Invalid number of months on deposit. Must be from 1 to 60.')
+            raise RequestValidationError('Invalid number of months on deposit. Must be from 1 to 60.')
         return v
 
     @field_validator("amount")
     @classmethod
     def validate_amount(cls, v: int) -> int:
         if not 10000 <= v <= 3000000:
-            raise ValueError('Invalid deposit amount. Must be from 10,000 to 3,000,000.')
+            raise RequestValidationError('Invalid deposit amount. Must be from 10,000 to 3,000,000.')
         return v
 
     @field_validator("rate")
     @classmethod
     def validate_rate(cls, v: float) -> float:
         if not 1 <= v <= 8:
-            raise ValueError('Incorrect interest on deposit. Must be from 1 to 8.')
+            raise RequestValidationError('Incorrect interest on deposit. Must be from 1 to 8.')
         return v
+
+
+class Deposit(DepositBase, table=True):
+    __tablename__ = 'deposit'
+    deposit_id: Optional[int] = Field(default=None, primary_key=True)
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import ORJSONResponse
+
 from db.db import init_db
+from db.models import Deposit, DepositBase
+from services.deposit import deposit_service
 from logger import app_logger
 import os
 from contextlib import asynccontextmanager
 
 import uvicorn
 import dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 dotenv.load_dotenv()
 
@@ -13,22 +18,29 @@ dotenv.load_dotenv()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app_logger.info('Start application')
-    init_db()
+    # init_db()
     yield
     app_logger.info('App stopped')
 
 
 app = FastAPI(lifespan=lifespan)
 
+# EXCEPTION HANDLER
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: [RequestValidationError|ValueError]):
+    error_msg = {'error': str(exc)}
+    return ORJSONResponse(error_msg, status_code=400)
+
+
+# VIEWS
+@app.post("/deposit_calculation", status_code=200)
+async def get_deposit_calculation(deposit: DepositBase):
+    return deposit_service.calculate_deposit(deposit)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
 
 
 if __name__ == "__main__":
